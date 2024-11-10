5e9a8a7bb17535100db7fd6d873b840a3819a62e
Bruno Russo <bruno@jumpstartmd.com>
2024-11-10 13:53:50 -0300

    feat: add main export for atom components

---
+++ components/atoms/index.ts
@@ -0,0 +1,15 @@
+import { Button } from './button/Button'
+import { CheckBox } from './checkbox/CheckBox'
+import { DatePicker } from './date-picker/DatePicker'
+import { HeaderBackButton } from './header-back-button/HeaderBackButton'
+import { InputCode } from './input-code/InputCode'
+import { Input } from './input/Input'
+
+export {
+  Button,
+  CheckBox,
+  DatePicker,
+  HeaderBackButton,
+  Input,
+  InputCode
+}
