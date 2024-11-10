6d652764499f725b98d5dbc30b9d970552716bd4
Bruno Russo <bruno@jumpstartmd.com>
2024-11-10 13:41:56 -0300

    feat: add animation to root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,14 @@ import { store } from "@/config/redux/store";
 import { SessionProvider } from "@/hooks/useSession";
 import { Slot, SplashScreen } from "expo-router";
 import { useEffect } from "react";
+import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import 'react-native-reanimated';
 import { Provider } from "react-redux";
 
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-
   useEffect(() => {
     setTimeout(() => {
       SplashScreen.hideAsync()
