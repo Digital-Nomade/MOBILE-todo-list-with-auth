759884ea07d6380a2b3469df08cd9a77fe17beac
Bruno Russo <bruno@jumpstartmd.com>
2024-11-10 14:09:21 -0300

    feat: add main export form organisms

---
+++ components/organisms/index.ts
@@ -0,0 +1,7 @@
+import { TabBarNavigation } from './TabBarNavigation/TabBarNavigation'
+import { TodoDetails } from './TodoModal/TodoDetails'
+
+export {
+  TabBarNavigation,
+  TodoDetails as TodoModal
+}
