4f2c4f4e47f62b41ad9e23a2ea8b589f45b278a8
Bruno Russo <bruno@jumpstartmd.com>
2024-11-10 14:11:18 -0300

    chore: add main styles

---
+++ constants/StyleGuide.ts
@@ -0,0 +1,23 @@
+export const StylesGuide = {
+  colors: {
+    primary: '#0E003A',
+    primaryTransparency: '#0E003ABB',
+    secondary: '#BF0066',
+    dangerDark: '#3E090D',
+    danger: '#F3434F',
+    dangerLight: '#EEB0B4',
+    success: '#7DE300',
+    alert: '#DBE300',
+    info: '#28DDFD',
+    white: '#FFFFFF',
+    black: '#333333',
+  },
+  fontSizes: {
+    sm: 12,
+    md: 16,
+    lg: 24,
+    xl: 32,
+    xll: 40,
+    xlll: 48,
+  }
+};
